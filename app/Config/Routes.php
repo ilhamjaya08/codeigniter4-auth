2e87f2ad88e3a5295a4c4fa5d3f44c118a47daa8
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-09-17 08:06:08 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,24 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+$routes->group('register', function($routes){
+    $routes->get('/', 'RegisterController::index');
+    $routes->post('/', 'RegisterController::store');
+});
+
+$routes->group('login', function ($routes) {
+    $routes->get('/', 'LoginController::index');
+    $routes->post('/', 'LoginController::login');
+});
+
+$routes->get('/dashboard', 'Dashboard::index');
+
+$routes->group('logout', function ($routes) {
+    $routes->get('/', 'LogoutController::index');
+});
